0806c6ff06f61ebbff0694681e67aa300b74c5ec
macnemoto <marcosj465@gmail.com>
2022-06-07 09:13:04 -0400

    :alien: update code api rout category and name

--- app.js
+++ app.js
@@ -1,64 +1,78 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var mysql      = require('mysql');
-const { json } = require('express/lib/response')
+var mysql = require("mysql");
+const { json } = require("express/lib/response");
 
 var connection = mysql.createConnection({
-  host     : 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
-  user     : 'bsale_test',
-  password : 'bsale_test',
-  database : 'bsale_test'
+  host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
+  user: "bsale_test",
+  password: "bsale_test",
+  database: "bsale_test",
 });
- 
-connection.connect(error => {
-    if (error) throw error;
-    console.log('Connected to database');
+
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("Connected to database");
 });
 setInterval(function () {
-    connection.query('SELECT 1');
+  connection.query("SELECT 1");
 }, 4000);
- 
-/* connection.query('SELECT * FROM product', function (error, results, fields) {
-  if (error) throw error;
-  //respuesta en json
-    return JSON.parse(JSON.stringify(results));
+// default route
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM product", function (error, results, fields) {
+    if (error) throw error;
 
+    let respuesta = JSON.parse(JSON.stringify(results));
+    /* connection.end(); */
+    res.send(respuesta);
+  });
 });
- 
-connection.end(); */
 
-
-
-
-
-
-app.get('/', (req, res) => {
-
-    connection.query('SELECT * FROM product', function (error, results, fields) {
-        if (error) throw error; 
-    
-        
-        let respuesta = JSON.parse(JSON.stringify(results));
-        /* connection.end(); */
-        res.send(respuesta)
-      
-      });
-       
-     
-    
-    
+// get product by id
+app.get("/:category", (req, res) => {
+    let category = req.params.category;
+    connection.query(
+        "SELECT * FROM product WHERE category = ?",
+        [category],
+        function (error, results, fields) {
+            if (error) throw error;
+            let respuesta = JSON.parse(JSON.stringify(results));
+            /* connection.end(); */
+            console.log(respuesta);
+            res.send(respuesta);
+        }
+    );
 }
+);
+
+// get product by id
+app.get("/product/:name", (req, res) => {
+    let name = req.params.name;
+    connection.query(
+        "SELECT * FROM product WHERE name like ?",
+        ['%' + name + '%'],
+        function (error, results, fields) {
+            if (error) throw error;
+            let respuesta = JSON.parse(JSON.stringify(results));
+            /* connection.end(); */
+            console.log(respuesta);
+            res.send(respuesta);
+        }
+    );
+}
+);
 
 
-  
-)
+
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
